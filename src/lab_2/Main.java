947a82659673f78a64bc575d56141e60ea7b744f
Vladimir <115186975+scobca@users.noreply.github.com>
2024-10-15 02:38:00 +0300

    Lab 2 - final version

--- src/lab_2/Main.java
+++ src/lab_2/Main.java
@@ -1,22 +1,27 @@
 package lab_2;
-import lab_2.pokemons.Sigilyph;
+import lab_2.pokemons.*;
+import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
-//        Battle b = new Battle();
-        Pokemon p1 = new Sigilyph("NEW_POKE", 1);
-        Pokemon p2 = new Pokemon("Хищник", 1);
-//        b.addAlly(p1);
-//        b.addFoe(p2);
-//        b.go();
+        Battle b = new Battle();
 
-        typesGet(p1);
-    }
+        Pokemon p1 = new Sigilyph("Летучка", 43);
+        Pokemon p2 = new Salandit("Ящерка", 37);
+        Pokemon p3 = new Salazzle("Токсинфея", 58);
+        Pokemon p4 = new Deino("Бульдозер", 32);
+        Pokemon p5 = new Zweilous("Двухглавик", 65);
+        Pokemon p6 = new Hydreigon("Горыныч", 37);
 
-    public static void typesGet(Pokemon pokemon) {
-        for (int i = 0; i < pokemon.getTypes().length; i++) {
-            System.out.println(pokemon.getTypes()[i]);
-        }
+        b.addAlly(p1);
+        b.addAlly(p2);
+        b.addAlly(p3);
+
+        b.addFoe(p4);
+        b.addFoe(p5);
+        b.addFoe(p6);
+
+        b.go();
     }
 }
