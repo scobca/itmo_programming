db0aad7a814911047311edbb037d7d8e3209f4da
Vladimir <scobca18@yandex.ru>
2024-11-27 00:25:12 +0300

    lab_3: new abstract structure for all entities

---
+++ src/lab_3/Entity.java
@@ -0,0 +1,17 @@
+package lab_3;
+
+import lab_3.entities.stats.Size;
+
+public abstract class Entity {
+    protected String name;
+
+    public Entity(String name) {
+        this.name = name;
+    }
+
+    public abstract Size setSize(int countableParam);
+
+    public int generateNumber() {
+        return (int) (Math.random() * 100);
+    }
+}
